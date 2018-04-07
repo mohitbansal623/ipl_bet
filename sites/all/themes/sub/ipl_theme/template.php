c46d38fd18a2c68c3063515bc0d2c0d0e8afb41e
mohitbansal623 <mohit.bansal623@gmail.com>
2018-04-07 18:35:51 +0530

    homepage design ready

--- sites/all/themes/sub/ipl_theme/template.php
+++ sites/all/themes/sub/ipl_theme/template.php
@@ -4,3 +4,14 @@
  * @file
  * The primary PHP file for this theme.
  */
+
+function ipl_theme_preprocess_page(&$variables) {
+  $path = path_to_theme();
+  kpr($path);
+  if (drupal_is_front_page()) {
+    drupal_add_css($path . '/stylesheets/homepage.css');
+    drupal_add_js($path . '/js/particles.js');
+    drupal_add_js($path . '/js/app.js');
+  }
+
+}
