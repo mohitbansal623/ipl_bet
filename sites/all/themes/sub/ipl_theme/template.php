81ef6e871d2cd2b323591f83c3e5ec319655cde9
Mukund <mukundatwork@gmail.com>
2018-04-09 00:16:03 +0530

    changes in forms

--- sites/all/themes/sub/ipl_theme/template.php
+++ sites/all/themes/sub/ipl_theme/template.php
@@ -7,10 +7,10 @@
 
 function ipl_theme_preprocess_page(&$variables) {
   $path = path_to_theme();
-  if (drupal_is_front_page()) {
+  // if (drupal_is_front_page()) {
     drupal_add_css($path . '/stylesheets/homepage.css');
     drupal_add_js($path . '/js/particles.js');
     drupal_add_js($path . '/js/app.js');
-  }
+  // }
 
 }
