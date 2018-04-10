50ccc62cb21156f53527a1c4855e2a2b27bdd147
Mukund <mukundatwork@gmail.com>
2018-04-11 00:46:28 +0530

    final changes

--- sites/all/themes/sub/ipl_theme/js/general.js
+++ sites/all/themes/sub/ipl_theme/js/general.js
@@ -1,3 +1,5 @@
+
 jQuery(document).ready(function(){
-    jQuery(".page-betting-form #betting-form div:first-child").not(".form-item").addClass("row");
+//    jQuery(".page-betting-form #betting-form div:first-child").not(".form-item").addClass("row");
+   jQuery(".page-pay-online-payment-status header.navbar").addClass("navbar-fixed-top");
 });
