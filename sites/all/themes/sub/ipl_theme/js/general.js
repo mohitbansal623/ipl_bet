0274b47dfc120d32076ef7a9301fff4986b5fc7c
mohitbansal623 <mohit.bansal623@gmail.com>
2018-04-13 22:59:10 +0530

    changes done as per the requirements

--- sites/all/themes/sub/ipl_theme/js/general.js
+++ sites/all/themes/sub/ipl_theme/js/general.js
@@ -1,4 +1,13 @@
 jQuery(document).ready(function(){
     // jQuery(".page-betting-form #betting-form div:first-child").not(".form-item").addClass("row");
     jQuery(".page-pay-online-payment-status header.navbar").addClass("navbar-fixed-top");
+
+    jQuery(".page-prediction-form input.form-radio").click(function() {
+      if (jQuery('.form-radio').hasClass('checked')){
+        jQuery('.form-radio').removeClass('checked');
+      } else {
+        jQuery('.form-radio').addClass('checked');
+        jQuery(".form-radio").prop("checked", false);
+      }
+    });
 });
